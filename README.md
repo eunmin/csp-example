ec44f96f7edeea33a81f50a486f888606f2679eb
eunmin <telepopsound@gmail.com>
2016-08-11 00:44:18 +0900

    예제 변경 및 주석 추가

--- README.md
+++ README.md
@@ -1,14 +1,49 @@
-# csp-example
+# CSP 예제
+```clojure
+;; 시나리오 :
+;;   url1 응답 본문 크기가 10 이상이면 url2 본문 크기를 더해서 출력한다.
+;;   10 이하이면 url1 응답 본문 크기를 출력한다.
 
-A Clojure library designed to ... well, that part is up to you.
+;; 1. 동기 버전 - 직관적이지만 비동기 처리보다 상대적으로 비효율적으로 자원을 활용한다.
+(defn- sync-http-get [url]
+  (print-thread-name)
+  @(http/get url1))
 
-## Usage
+(defn sync-sum-content-length []
+  (println "result:"
+    (let [resp1 (sync-http-get url1)
+          resp1-len (content-length resp1)]
+      (if (> resp1-len 10)
+        (+ resp1-len (content-length (sync-http-get url2)))
+        resp1-len))))
 
-FIXME
+;; 2. 콜백을 이용한 비동기 버전 - 콜백 때문에 흐름이 눈에 들어오지 않는다.
+(defn async-sum-content-length []
+  (http/get url1
+    (fn [resp1]
+      (print-thread-name)
+      (let [resp1-len (content-length resp1)]
+        (if (> resp1-len 10)
+          (http/get url2
+            (fn [resp2]
+              (print-thread-name)
+              (println "result:" (+ resp1-len (content-length resp2)))))
+          (println "result:" resp1-len))))))
 
-## License
+;; 3. CSP를 이용한 비동기 버전 - 직관적인 흐름(마치 동기적으로 처리되는 형태)과 비동기적 장점을 모두 가진다.
+(defn- csp-http-get [url]
+  (let [ch (chan)]
+    (print-thread-name)
+    (http/get url #(put! ch %))
+    ch))
 
-Copyright © 2016 FIXME
-
-Distributed under the Eclipse Public License either version 1.0 or (at
-your option) any later version.
+(defn csp-sum-content-length []
+  (println "result:"
+    (<!!
+      (go
+        (let [resp1 (<! (csp-http-get url1))
+              resp1-len (content-length resp1)]
+          (if (> resp1-len 10)
+            (+ resp1-len (content-length (<! (csp-http-get url2))))
+            resp1-len))))))
+```
--- src/csp_example/core.clj
+++ src/csp_example/core.clj
@@ -1,6 +1,6 @@
 (ns csp-example.core
   (:require [org.httpkit.client :as http]
-            [clojure.core.async :refer [chan put! go <!]]))
+            [clojure.core.async :refer [chan put! go <! <!!]]))
 
 (def url1 "http://www.bbc.com/sport/football/25611509")
 
@@ -15,27 +15,37 @@
 (defn- content-length [response]
   (parse-int (-> response :headers :content-length)))
 
-(defn- print-result [resp1 resp2]
-  (println "sum length:" (+ (content-length resp1) (content-length resp2))))
+;; 시나리오 :
+;;   url1 응답 본문 크기가 10 이상이면 url2 본문 크기를 더해서 출력한다.
+;;   10 이하이면 url1 응답 본문 크기를 출력한다.
 
+;; 1. 동기 버전 - 직관적이지만 비동기 처리보다 상대적으로 비효율적으로 자원을 활용한다.
 (defn- sync-http-get [url]
   (print-thread-name)
   @(http/get url1))
 
 (defn sync-sum-content-length []
-  (let [resp1 (sync-http-get url1)
-        resp2 (sync-http-get url2)]
-    (print-result resp1 resp2)))
+  (println "result:"
+    (let [resp1 (sync-http-get url1)
+          resp1-len (content-length resp1)]
+      (if (> resp1-len 10)
+        (+ resp1-len (content-length (sync-http-get url2)))
+        resp1-len))))
 
+;; 2. 콜백을 이용한 비동기 버전 - 콜백 때문에 흐름이 눈에 들어오지 않는다.
 (defn async-sum-content-length []
   (http/get url1
     (fn [resp1]
       (print-thread-name)
-      (http/get url2
-        (fn [resp2]
-          (print-thread-name)
-          (print-result resp1 resp2))))))
+      (let [resp1-len (content-length resp1)]
+        (if (> resp1-len 10)
+          (http/get url2
+            (fn [resp2]
+              (print-thread-name)
+              (println "result:" (+ resp1-len (content-length resp2)))))
+          (println "result:" resp1-len))))))
 
+;; 3. CSP를 이용한 비동기 버전 - 직관적인 흐름(마치 동기적으로 처리되는 형태)과 비동기적 장점을 모두 가진다.
 (defn- csp-http-get [url]
   (let [ch (chan)]
     (print-thread-name)
@@ -43,7 +53,11 @@
     ch))
 
 (defn csp-sum-content-length []
-  (go
-    (let [resp1 (<! (csp-http-get url1))
-          resp2 (<! (csp-http-get url2))]
-      (print-result resp1 resp2))))
+  (println "result:"
+    (<!!
+      (go
+        (let [resp1 (<! (csp-http-get url1))
+              resp1-len (content-length resp1)]
+          (if (> resp1-len 10)
+            (+ resp1-len (content-length (<! (csp-http-get url2))))
+            resp1-len))))))
